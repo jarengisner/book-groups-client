015659ff4622d4672a9c99896b7473454e4b79d7
jarengisner <124743127+jarengisner@users.noreply.github.com>
2024-03-05 18:20:34 -0800

    Added search feature to the tags within create group, and some styling

--- src/components/create-group/create-group.component.jsx
+++ src/components/create-group/create-group.component.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+
 import '../../index.css';
 
 export const CreateGroup = ({ user, tags }) => {
@@ -7,6 +9,7 @@ export const CreateGroup = ({ user, tags }) => {
   const [bio, setBio] = useState('');
   const [initialPost, setInitialPost] = useState('');
   const [groupTags, setGroupTags] = useState([]);
+  const [query, setQuery] = useState('');
 
   //sets state for each property of a group
   const nameChangeHandle = (event) => {
@@ -25,6 +28,14 @@ export const CreateGroup = ({ user, tags }) => {
     } else {
       setGroupTags((prevTags) => [...prevTags, tag]);
     }
+  };
+
+  const querySearchHandle = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const queryCreateHandle = (tag) => {
+    setGroupTags((prevTags) => [...prevTags, tag]);
   };
 
   //handles creation of group fetch requests
@@ -55,13 +66,18 @@ export const CreateGroup = ({ user, tags }) => {
       {/* look for details about what exactly we added in the post creation */}
       <div>
         <h1>Select tags for your group</h1>
+        <input
+          type='text'
+          value={query}
+          onChange={querySearchHandle}
+          placeholder='Search or create tag...'
+        />
         {groupTags.length === 0 ? (
           <p>No tags selected so far</p>
         ) : (
           <div>
             {groupTags.map((t) => (
               <Button
-                key={t}
                 variant='outline-secondary'
                 onClick={() => tagsHandler(t)}
                 className='selected-tags'
@@ -71,16 +87,94 @@ export const CreateGroup = ({ user, tags }) => {
             ))}
           </div>
         )}
-        {tags.length > 0 ? (
-          tags.map((t) => (
-            <Button key={t} onClick={() => tagsHandler(t)}>
-              {t}
-            </Button>
-          ))
-        ) : (
-          <h1>Loading.....</h1>
-        )}
+        <div className='choices-container'>
+          {query && !tags.includes(query.toLowerCase()) && (
+            <Row>
+              <Button
+                onClick={() => queryCreateHandle(query)}
+                style={{ marginBottom: '20px', width: '300px' }}
+              >
+                Create {query}
+              </Button>
+            </Row>
+          )}
+          <div>
+            {/* <Row>
+              {tags.length > 0 ? (
+                query ? (
+                  tags
+                    .filter((tag) => tag.includes(query.toLowerCase()))
+                    .map((t) => (
+                      <Col md={3} style={{ marginTop: 8 }}>
+                        <Button key={t} onClick={() => tagsHandler(t)}>
+                          {t}
+                        </Button>
+                      </Col>
+                    ))
+                ) : (
+                  tags.map((t) => (
+                    <Col md={3} style={{ marginTop: 8 }}>
+                      <Button key={t} onClick={() => tagsHandler(t)}>
+                        {t}
+                      </Button>
+                    </Col>
+                  ))
+                )
+              ) : (
+                <h1>Loading.....</h1>
+              )}
+            </Row> */}
+
+            {tags.length > 0 ? (
+              <div>
+                {query ? (
+                  <Row>
+                    {tags
+                      .filter((tag) => tag.includes(query.toLowerCase()))
+                      .map((t) => (
+                        <Col key={t} style={{ marginBottom: 8 }}>
+                          <Button
+                            onClick={() => tagsHandler(t)}
+                            style={{ width: '125px' }}
+                          >
+                            {t}
+                          </Button>
+                        </Col>
+                      ))}
+                  </Row>
+                ) : (
+                  <Row>
+                    {tags.map((t) => (
+                      <Col key={t} md={3} style={{ marginBottom: 8 }}>
+                        <Button
+                          onClick={() => tagsHandler(t)}
+                          style={{ width: '125px' }}
+                        >
+                          {t}
+                        </Button>
+                      </Col>
+                    ))}
+                  </Row>
+                )}
+              </div>
+            ) : (
+              <h1>Loading.....</h1>
+            )}
+          </div>
+        </div>
       </div>
     </Container>
   );
 };
+
+/* {tags.length > 0 ? (
+    tags.map((t) => (
+      <Col md={3} style={{ marginTop: 8 }}>
+        <Button key={t} onClick={() => tagsHandler(t)}>
+          {t}
+        </Button>
+      </Col>
+    ))
+  ) : (
+    <h1>Loading.....</h1>
+  )} */
