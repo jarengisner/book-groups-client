bf541439d51ad8b606c83815dcfd7fd92b18c8a5
jarengisner <124743127+jarengisner@users.noreply.github.com>
2024-03-21 17:04:07 -0700

    Worked on editing form, as well as some small styling changes

--- src/components/group-list-component/group-editing.component.jsx
+++ src/components/group-list-component/group-editing.component.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX, faImage } from '@fortawesome/free-solid-svg-icons';
 //CSS import
@@ -14,9 +14,13 @@ export const EditGroups = ({ group, tags, closeHandler }) => {
   const [showGroupPhotoUpload, setShowGroupPhotoUpload] = useState(false);
   const [groupPhoto, setGroupPhoto] = useState(group.groupImg);
 
-  const nameEditHandler = () => {};
+  const nameEditHandler = (e) => {
+    setEditedName(e.target.value);
+  };
 
-  const descriptionEditHandler = () => {};
+  const descriptionEditHandler = (e) => {
+    setEditedDescription(e.target.value);
+  };
 
   const tagEditHandler = () => {};
 
@@ -32,24 +36,60 @@ export const EditGroups = ({ group, tags, closeHandler }) => {
     setGroupPhoto(url);
   };
 
+  const submitChangesHandle = () => {
+    fetch(`http://localhost:8080/clubs/${group.name}/details`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        name: editedName,
+        description: editedDescription,
+        tags: editedTags,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        //This is where we can pass the new updated user back to the user-list so that it renders instantly
+        console.log(data);
+      });
+  };
+
   return (
     <div className='group-edit-inner-container'>
-      <Row>
-        <Col md={4}>
-          <img src={groupPhoto} alt='group profile' />
-          <button onClick={() => editPhotoHandler()}>
-            <FontAwesomeIcon icon={faImage} className='imageIcon' />
-          </button>
-        </Col>
-        <Col md={7}>
-          <h1>{group.name}</h1>
-          <p>{group.description}</p>
-        </Col>
+      {/* <Row>
         <Col md={1}>
           <button className='close-button' onClick={() => closeHandler()}>
             <FontAwesomeIcon icon={faX} className='x-icon' />
           </button>
         </Col>
+      </Row> */}
+      <Row style={{ marginTop: '2%' }}>
+        <div className='standard-row-flex'>
+          <Col md={4}>
+            <div className='standard-column-flex'>
+              <img
+                src={groupPhoto}
+                alt='group profile'
+                className='editGroupPic'
+              />
+              <button onClick={() => editPhotoHandler()} className='img-button'>
+                <FontAwesomeIcon icon={faImage} className='imageIcon' />
+              </button>
+            </div>
+          </Col>
+          <Col md={4}>
+            <h1>{group.name}</h1>
+            <p>{group.description}</p>
+          </Col>
+          <Col md={4}>
+            <div className='close-button-position'>
+              <button className='close-button' onClick={() => closeHandler()}>
+                <FontAwesomeIcon icon={faX} className='x-icon' />
+              </button>
+            </div>
+          </Col>
+        </div>
       </Row>
       <div>
         {showGroupPhotoUpload ? (
@@ -59,26 +99,33 @@ export const EditGroups = ({ group, tags, closeHandler }) => {
             imageUploadHandler={imageUploadHandler}
           />
         ) : (
-          <>
-            <Row>
-              <h2>Form placeholder</h2>
+          <div className='edit-form-div'>
+            <Row className='group-edit-input'>
+              <h2>Edit Name</h2>
+              <input
+                type='text'
+                placeholder={group.name}
+                value={editedName}
+                onChange={nameEditHandler}
+              ></input>
+            </Row>
+            <Row className='group-edit-input'>
+              <h2>Edit Description</h2>
+              <textarea
+                placeholder={group.description}
+                rows={3}
+                style={{ marginBottom: '5%' }}
+                value={editedDescription}
+                onChange={descriptionEditHandler}
+              ></textarea>
             </Row>
             <Row>
-              <h2>Form placeholder</h2>
+              <h1>Need to make tags section here</h1>
             </Row>
             <Row>
-              <h2>Form placeholder</h2>
+              <Button onClick={submitChangesHandle}>Submit Changes</Button>
             </Row>
-            <Row>
-              <h2>Form placeholder</h2>
-            </Row>
-            <Row>
-              <h2>Form placeholder</h2>
-            </Row>
-            <Row>
-              <h2>Form placeholder</h2>
-            </Row>
-          </>
+          </div>
         )}
       </div>
     </div>
